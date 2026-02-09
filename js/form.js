a653ce2398c0bbf45b3cc1622a773fe47c612a7a
moonhouse-80san <mbg1346@gmail.com>
2026-02-09 14:28:57 +0900

    수정시 상단으로 이동

--- js/form.js
+++ js/form.js
@@ -411,7 +411,7 @@ function editMember(index) {
 	setTimeout(() => {
 		const nameInput = document.getElementById('name');
 		if (nameInput) {
-			nameInput.scrollIntoView({ behavior: 'smooth', block: 'start' });
+			nameInput.scrollIntoView({  top: -30, behavior: 'smooth'  });
 			
 			// 포커스 및 선택
 			setTimeout(() => {
--- js/member.js
+++ js/member.js
@@ -745,7 +745,7 @@ function editMember(index) {
 	setTimeout(() => {
 		const nameInput = document.getElementById('name');
 		if (nameInput) {
-			nameInput.scrollIntoView({ behavior: 'smooth', block: 'start' });
+			nameInput.scrollIntoView({  top: -30, behavior: 'smooth'  });
 			
 			// 포커스 및 선택
 			setTimeout(() => {
